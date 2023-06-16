2327411356cd6696209b3da1693c4fb719a262be
tomiaa <1178852449@qq.com>
2023-06-16 17:57:02 +0800

    feat: algolia配置

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -129,11 +129,57 @@ export default defineConfig({
     ],
   ],
   themeConfig: {
-    // algolia: {
-    //   appId: "FGZRHFTTSH",
-    //   apiKey: "1995152b867b6ed3a2b75c50bc8e9c81",
-    //   indexName: "kuangyx",
-    // },
+    search: {
+      provider: "algolia",
+      options: {
+        appId: "1AS9BEA8JY",
+        apiKey: "6564a713013f5f79664a8be62e7f3235",
+        indexName: "kuangyx",
+        locales: {
+          zh: {
+            placeholder: "搜索文档",
+            translations: {
+              button: {
+                buttonText: "搜索文档",
+                buttonAriaLabel: "搜索文档",
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: "清除查询条件",
+                  resetButtonAriaLabel: "清除查询条件",
+                  cancelButtonText: "取消",
+                  cancelButtonAriaLabel: "取消",
+                },
+                startScreen: {
+                  recentSearchesTitle: "搜索历史",
+                  noRecentSearchesText: "没有搜索历史",
+                  saveRecentSearchButtonTitle: "保存至搜索历史",
+                  removeRecentSearchButtonTitle: "从搜索历史中移除",
+                  favoriteSearchesTitle: "收藏",
+                  removeFavoriteSearchButtonTitle: "从收藏中移除",
+                },
+                errorScreen: {
+                  titleText: "无法获取结果",
+                  helpText: "你可能需要检查你的网络连接",
+                },
+                footer: {
+                  selectText: "选择",
+                  navigateText: "切换",
+                  closeText: "关闭",
+                  searchByText: "搜索提供者",
+                },
+                noResultsScreen: {
+                  noResultsText: "无法找到相关结果",
+                  suggestedQueryText: "你可以尝试查询",
+                  reportMissingResultsText: "你认为该查询应该有结果？",
+                  reportMissingResultsLinkText: "点击反馈",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
     outlineTitle: "此页目录",
     lastUpdatedText: "最后更新时间",
     outline: [2, 6],
