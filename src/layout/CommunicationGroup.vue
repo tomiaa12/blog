a64bab4cae4ac874935bc0cbc8a36e55d2224370
tomiaa <1178852449@qq.com>
2023-07-24 10:38:15 +0800

    fix: 微信群二维码更新

--- src/layout/CommunicationGroup.vue
+++ src/layout/CommunicationGroup.vue
@@ -1,4 +1,19 @@
 <template>
+  <h2>微信群</h2>
+
+  微信2群
+  <img
+    src="./CommunicationGroup/img/wxq.jpg"
+    alt="微信群"
+  />
+
+  <h3>群二维码失效时添加好友拉群，1群人多只能通过好友邀请加入</h3>
+  <img
+    src="./CommunicationGroup/img/wx.jpg"
+    alt="微信好友"
+  />
+  <p>微信添加好友加群，添加时请备注前端</p>
+
   <h2>QQ</h2>
   <img
     src="./CommunicationGroup/img/qq.png"
@@ -10,19 +25,4 @@
   >
     点我一键加群
   </a>
-
-  <h2>微信</h2>
-
-  <h3>微信群</h3>
-  <img
-    src="./CommunicationGroup/img/wxq.jpg"
-    alt=""
-  />
-
-  <h3>群二维码失效时添加好友拉群</h3>
-  <img
-    src="./CommunicationGroup/img/wx.jpg"
-    alt="微信"
-  />
-  <p>微信添加好友加群，添加时请备注前端</p>
 </template>
