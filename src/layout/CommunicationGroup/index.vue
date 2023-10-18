ffc1b2e5d8b08502628bf24960db27b078027664
tomiaa <1178852449@qq.com>
2023-10-18 17:06:16 +0800

    docs: 群功能一览

--- src/layout/CommunicationGroup/index.vue
+++ src/layout/CommunicationGroup/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <h2>微信1群↓</h2>
+  <h2>微信群↓</h2>
   <img
-    src="./img/wxq1.jpg"
+    src="./img/wxq1.png"
     alt="微信群1"
+  />
+  <h2>微信备用群↓</h2>
+  <img
+    src="./img/wxq2.png"
+    alt="微信群2"
   />
 
   <h2>邀请码失效时添加好友拉群↓</h2>
@@ -22,8 +27,96 @@
   >
     点我一键加群
   </a>
-</template>
 
+  <h2>群机器人功能一览</h2>
+  <h3>GPT回答</h3>
+  <img
+    src="./img/01.png"
+    alt="GPT回答"
+  />
+  <h3>猜电影</h3>
+  <img
+    src="./img/02.png"
+    alt="猜电影"
+  />
+  <h3>猜音乐</h3>
+  <img
+    src="./img/03.png"
+    alt="猜音乐"
+  />
+  <h3>猜英雄联盟</h3>
+  <img
+    src="./img/04.png"
+    alt="猜英雄联盟"
+  />
+  <h3>早报</h3>
+  <img
+    src="./img/05.png"
+    alt="早报"
+  />
+  <h3>一句</h3>
+  <img
+    src="./img/06.png"
+    alt="一句"
+  />
+  <h3>一言</h3>
+  <img
+    src="./img/07.png"
+    alt="一言"
+  />
+  <h3>彩虹屁</h3>
+  <img
+    src="./img/08.png"
+    alt="彩虹屁"
+  />
+  <h3>毒鸡汤</h3>
+  <img
+    src="./img/09.png"
+    alt="毒鸡汤"
+  />
+  <h3>入群测验</h3>
+  <img
+    src="./img/10.png"
+    alt="入群测验"
+  />
+  <h3>舔狗日记</h3>
+  <img
+    src="./img/11.png"
+    alt="舔狗日记"
+  />
+  <h3>网易云热评</h3>
+  <img
+    src="./img/12.png"
+    alt="网易云热评"
+  />
+  <h3>猜奥特曼</h3>
+  <img
+    src="./img/13.png"
+    alt="猜奥特曼"
+  />
+  <h3>二次元浓度测试</h3>
+  <img
+    src="./img/14.png"
+    alt="二次元浓度测试"
+  />
+  <h3>画图 + 空格 + 内容</h3>
+  <img
+    src="./img/15.png"
+    alt="画图"
+  />
+  <h3>翻译 + 空格 + 文字</h3>
+  <img
+    src="./img/16.png"
+    alt="翻译"
+  />
+
+</template>
+<script setup lang="ts">
+import { useData } from "vitepress"
+const data = useData()
+
+console.log(data)
+</script>
 <style scoped>
 h2 {
   margin-top: 1em;
