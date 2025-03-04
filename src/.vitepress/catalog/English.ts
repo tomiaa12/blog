df92e100d067d614423beb927af6ab35238884cf
tomiaa <1178852449@qq.com>
2025-03-04 17:43:33 +0800

    docs: 新增 English 文章

--- src/.vitepress/catalog/English.ts
+++ src/.vitepress/catalog/English.ts
@@ -11,6 +11,11 @@ export const English = [
       { text: "定语", link: "/docs/English/定语" },
       { text: "同位语", link: "/docs/English/同位语" },
       { text: "基本句型", link: "/docs/English/基本句型" },
+      { text: "名词与分类", link: "/docs/English/名词与分类" },
+      { text: "名词单数变复数规则", link: "/docs/English/名词单数变复数规则" },
+      { text: "不可数名词与量词", link: "/docs/English/不可数名词与量词" },
+      { text: "名词的作用", link: "/docs/English/名词的作用" },
+      { text: "名词所有格", link: "/docs/English/名词所有格" },
     ]
   }
 ]
